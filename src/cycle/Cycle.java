c407c9d472be4e6eadf3aefcaf2a9a0eda1114f1
notebook <notebook@notebook-PC>
2016-11-25 12:56:40 +0200

    gvjvh

---
+++ src/cycle/Cycle.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cycle;
+
+/**
+ *
+ * @author notebook
+ */
+public class Cycle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //задание 1
+        int i=1000;
+        while(i<=9999){
+            System.out.println(i);
+            i=i+3;
+        }
+        
+        //Задание 2
+       
+    }
+    
+}
